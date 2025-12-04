9667c09c93a29dfb08bf0ba024024db20ed05307
Leo Barreiro <a.leo.barreiro@gmail.com>
2025-12-04 03:27:09 +0000

    feat: Flush events asynchronously reducing memory usage

--- src/Profiler/trace_collector.cpp
+++ src/Profiler/trace_collector.cpp
@@ -7,29 +7,49 @@
 #include <serialization.hpp>
 
 #include <sstream>
+#include <thread>
+
+void flush_events(Tracer::FileExporter& exporter, std::vector<std::string> events)
+{
+    for (const auto& raw_event : events) {
+        Tracer::ChromeEvent event {};
+        std::stringstream ss(raw_event);
+        ss >> event;
+        exporter.push_trace(event);
+    }
+}
+
+void async_flush_events(Tracer::FileExporter& exporter, std::vector<std::string>&& raw_events)
+{
+    std::vector<std::string> events_to_flush;
+    events_to_flush.reserve(raw_events.size());
+    events_to_flush.swap(raw_events);
+
+    std::thread([&exporter, events_to_flush = std::move(events_to_flush)]() {
+        flush_events(exporter, std::move(events_to_flush));
+    }).detach();
+}
 
 void run(IPC::PipeServer& server, std::string_view output_file)
 {
-    // File Exporter
     Tracer::FileExporter& exporter = Tracer::FileExporter::instance(output_file);
 
-    // TODO(lbarreiro): Buffer events and flush periodically to avoid high memory usage
     std::vector<std::string> raw_events {};
-    raw_events.reserve(1024);
+    raw_events.reserve(100);
 
-    const auto message_handler = [&raw_events](const IPC::Message& msg) {
+    const auto message_handler = [&](const IPC::Message& msg) {
         // std::println("Received message:\n{}", IPC::to_string(msg));
         raw_events.emplace_back(msg.body);
+        if (raw_events.size() >= raw_events.capacity()) {
+            async_flush_events(exporter, std::move(raw_events));
+        }
     };
 
-    const auto stop_handler = [&exporter, &raw_events]() {
-        std::println("Trace collector shutdown complete");
-        for (const auto& raw_event : raw_events) {
-            Tracer::ChromeEvent event {};
-            std::stringstream ss(raw_event);
-            ss >> event;
-            exporter.push_trace(event);
+    const auto stop_handler = [&]() {
+        if (!raw_events.empty()) {
+            flush_events(exporter, raw_events);
         }
+        std::println("Trace collector shutdown complete");
     };
 
     server.run(message_handler, stop_handler);
