58a4e3361debee74ff1867c7e31cdd51d94ec342
Leo Barreiro <a.leo.barreiro@gmail.com>
2025-12-22 23:12:14 -0300

    test: support collect traces from forked PID to file

--- src/TraceCollector/tests/profiler_file_test.cpp
+++ src/TraceCollector/tests/profiler_file_test.cpp
@@ -8,32 +8,32 @@
 #include <unistd.h>
 #include <vector>
 
-// Simple function with IPC_TRACE_FN
+// Simple function with TRACE_FN
 void simple_function()
 {
-    IPC_TRACE_FN();
+    TRACE_FN();
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 // Function with custom category
 void categorized_function()
 {
-    IPC_TRACE_FN_CAT("computation");
+    TRACE_FN_CAT("computation");
     std::this_thread::sleep_for(std::chrono::milliseconds(15));
 }
 
 // Nested function calls
 void inner_function()
 {
-    IPC_TRACE_FN_CAT("nested");
+    TRACE_FN_CAT("nested");
     std::this_thread::sleep_for(std::chrono::milliseconds(5));
 }
 
 void middle_function()
 {
-    IPC_TRACE_FN_CAT("nested");
+    TRACE_FN_CAT("nested");
     {
-        IPC_TRACE_SCOPE_CAT("middle_processing", "nested");
+        TRACE_SCOPE_CAT("middle_processing", "nested");
         std::this_thread::sleep_for(std::chrono::milliseconds(8));
         inner_function();
     }
@@ -41,14 +41,14 @@ void middle_function()
 
 void outer_function()
 {
-    IPC_TRACE_FN_CAT("nested");
+    TRACE_FN_CAT("nested");
     {
-        IPC_TRACE_SCOPE("outer_setup");
+        TRACE_SCOPE("outer_setup");
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
     middle_function();
     {
-        IPC_TRACE_SCOPE("outer_cleanup");
+        TRACE_SCOPE("outer_cleanup");
         std::this_thread::sleep_for(std::chrono::milliseconds(3));
     }
 }
@@ -56,7 +56,7 @@ void outer_function()
 // Recursive function
 void recursive_fibonacci(int n, int depth = 0)
 {
-    IPC_TRACE_FN_CAT("recursion");
+    TRACE_FN_CAT("recursion");
     if (n <= 1 || depth > 5) {
         return;
     }
@@ -67,23 +67,23 @@ void recursive_fibonacci(int n, int depth = 0)
 // Function with multiple scopes
 void function_with_scopes()
 {
-    IPC_TRACE_FN_CAT("scopes");
+    TRACE_FN_CAT("scopes");
 
     {
-        IPC_TRACE_SCOPE("initialization_phase");
+        TRACE_SCOPE("initialization_phase");
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     {
-        IPC_TRACE_SCOPE_CAT("processing_phase", "scopes");
+        TRACE_SCOPE_CAT("processing_phase", "scopes");
         for (int i = 0; i < 3; ++i) {
-            IPC_TRACE_SCOPE("loop_iteration");
+            TRACE_SCOPE("loop_iteration");
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
     }
 
     {
-        IPC_TRACE_SCOPE_CAT("finalization_phase", "scopes");
+        TRACE_SCOPE_CAT("finalization_phase", "scopes");
         std::this_thread::sleep_for(std::chrono::milliseconds(8));
     }
 }
@@ -92,7 +92,7 @@ void function_with_scopes()
 std::future<int> async_computation(int value)
 {
     return std::async(std::launch::async, [value]() {
-        IPC_TRACE_SCOPE_CAT("async_task", "async");
+        TRACE_SCOPE_CAT("async_task", "async");
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
         return value * 2;
     });
@@ -100,19 +100,19 @@ std::future<int> async_computation(int value)
 
 void test_async_operations()
 {
-    IPC_TRACE_FN_CAT("async");
+    TRACE_FN_CAT("async");
 
     std::vector<std::future<int>> futures;
 
     {
-        IPC_TRACE_SCOPE_CAT("launching_tasks", "async");
+        TRACE_SCOPE_CAT("launching_tasks", "async");
         for (int i = 0; i < 4; ++i) {
             futures.push_back(async_computation(i));
         }
     }
 
     {
-        IPC_TRACE_SCOPE_CAT("waiting_for_results", "async");
+        TRACE_SCOPE_CAT("waiting_for_results", "async");
         for (auto& fut : futures) {
             fut.get();
         }
@@ -122,15 +122,15 @@ void test_async_operations()
 // Thread function
 void thread_worker(int /* thread_id */)
 {
-    IPC_TRACE_FN_CAT("threads");
+    TRACE_FN_CAT("threads");
 
     {
-        IPC_TRACE_SCOPE_CAT("thread_initialization", "threads");
+        TRACE_SCOPE_CAT("thread_initialization", "threads");
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 
     for (int i = 0; i < 3; ++i) {
-        IPC_TRACE_SCOPE("thread_work_iteration");
+        TRACE_SCOPE("thread_work_iteration");
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         if (i % 2 == 0) {
@@ -139,26 +139,26 @@ void thread_worker(int /* thread_id */)
     }
 
     {
-        IPC_TRACE_SCOPE_CAT("thread_cleanup", "threads");
+        TRACE_SCOPE_CAT("thread_cleanup", "threads");
         std::this_thread::sleep_for(std::chrono::milliseconds(3));
     }
 }
 
 void test_multithreading()
 {
-    IPC_TRACE_FN_CAT("threads");
+    TRACE_FN_CAT("threads");
 
     std::vector<std::thread> threads;
 
     {
-        IPC_TRACE_SCOPE_CAT("spawning_threads", "threads");
+        TRACE_SCOPE_CAT("spawning_threads", "threads");
         for (int i = 0; i < 4; ++i) {
             threads.emplace_back(thread_worker, i);
         }
     }
 
     {
-        IPC_TRACE_SCOPE_CAT("joining_threads", "threads");
+        TRACE_SCOPE_CAT("joining_threads", "threads");
         for (auto& t : threads) {
             t.join();
         }
@@ -168,14 +168,14 @@ void test_multithreading()
 // Combined scenario: threads calling recursive functions
 void thread_with_recursion(int /* id */)
 {
-    IPC_TRACE_FN_CAT("combined");
+    TRACE_FN_CAT("combined");
     outer_function();
     recursive_fibonacci(6);
 }
 
 void test_combined_scenario()
 {
-    IPC_TRACE_FN_CAT("combined");
+    TRACE_FN_CAT("combined");
 
     std::vector<std::thread> threads;
     for (int i = 0; i < 2; ++i) {
@@ -189,17 +189,17 @@ void test_combined_scenario()
 
 void test_fork()
 {
-    IPC_TRACE_FN_CAT("Fork");
+    TRACE_FN_CAT("Fork");
     pid_t pid = fork();
     if (pid < 0) {
         return std::println(stderr, "Fork failed");
     }
     const auto child_process = []() {
-        IPC_TRACE_SCOPE_CAT("child_process", "fork");
+        TRACE_SCOPE_CAT("child_process", "fork");
         std::println("Child process running: {}", getpid());
     };
     const auto parent_process = [pid]() {
-        IPC_TRACE_SCOPE_CAT("parent_process", "fork");
+        TRACE_SCOPE_CAT("parent_process", "fork");
         std::println("Parent process ({}), child pid: {}", getpid(), pid);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     };
@@ -208,12 +208,12 @@ void test_fork()
 
 int main(int argc, char* argv[])
 {
-    const char* pipe_path = (argc < 2) ? "/tmp/tracer-trace_pipe_test.pipe" : argv[2];
+    const char* pipe_path = (argc < 2) ? "/tmp/tracer-trace_file_test.json" : argv[2];
 
     std::println("Starting profiler test...\n");
-    IPC_TRACE_SETUP(pipe_path);
+    TRACE_SETUP(pipe_path);
 
-    IPC_TRACE_FN();
+    TRACE_FN();
 
     for (int i : std::views::iota(1, 6)) {
         std::ignore = i;
